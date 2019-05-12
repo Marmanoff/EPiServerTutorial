3ce0e8a98f2ed711c9aafd75eabd11d5354e061e
Mark Mansour <mark.mansour@hotmail.se>
2019-05-13 01:05:57 +0200

    Page Type Tutorial
    https://youtu.be/UBoBf0s872g

---
+++ EPiServerTutorial1/Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using EPiServerTutorial1.Models.Pages;
+
+namespace EPiServerTutorial1.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ EPiServerTutorial1/Models/Pages/StartPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiServerTutorial1.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "5df26368-9f8c-4d3d-be03-08980035f9e9", Description = "")]
+    public class StartPage : PageData
+    {
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
