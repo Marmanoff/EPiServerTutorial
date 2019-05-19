560302f4d30c8936d35a39a196275c68a0aa9118
Mark Mansour <mark.mansour@hotmail.se>
2019-05-19 18:22:15 +0200

    Revert "EPiServer for Developers - 5# Generic types and View Models"
    This reverts commit f054c09435a6811ed6dc94f74aa24ecee257ba07.

--- EPiServerTutorial1/Controllers/StartPageController.cs
+++ EPiServerTutorial1/Controllers/StartPageController.cs
@@ -4,11 +4,8 @@ using System.Web.Mvc;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.Framework.DataAnnotations;
-using EPiServer.UI.Report;
 using EPiServer.Web.Mvc;
 using EPiServerTutorial1.Models.Pages;
-using EPiServerTutorial1.Models.ViewModels;
-
 
 namespace EPiServerTutorial1.Controllers
 {
@@ -16,8 +13,7 @@ namespace EPiServerTutorial1.Controllers
     {
         public ActionResult Index(StartPage currentPage)
         {
-            var model = PageViewModel.Create(currentPage);
-            return View(model);
+            return View(currentPage);
         }
     }
 }
